5e5b35be29ec82198341bb38877f629d9d73d178
ruthiesu <52277069+ruthiesu@users.noreply.github.com>
2025-04-19 17:05:57 +0300

    initial version

---
+++ day02/circle.py
@@ -0,0 +1,5 @@
+radius = float(input("Enter the radius of the circle: "))
+area = 3.14 * (radius**2)
+print(f"The area of the circle is: {area}")
+perimeter = 2 * 3.14 * radius
+print(f"The perimeter of the circle is: {perimeter}")

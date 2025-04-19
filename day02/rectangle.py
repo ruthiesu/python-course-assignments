d355a21af5ae21874311ee60529d410e9897633b
ruthiesu <52277069+ruthiesu@users.noreply.github.com>
2025-04-19 17:12:48 +0300

    initial version

---
+++ day02/rectangle.py
@@ -0,0 +1,8 @@
+height = float(input("Enter rectangle height: "))
+width = float(input("Enter rectangle width: "))
+
+area = height * width
+print(f"The area of the rectangle is {area}")
+
+perimeter = (2 * width) + (2 * height)
+print(f"The perimeter of the rectangle is {perimeter}")
